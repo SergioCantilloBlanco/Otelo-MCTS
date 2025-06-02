6c3540ea9bb86a3d6885748a5990c4e219ab3a6a
SergioCantilloBlanco <14sergiocb@gmail.com>
2025-06-02 22:00:33 +0200

    feat: :sparkles: neuron to algorithm

--- src/othello/neuronal_UCT_othello_agent.py
+++ src/othello/neuronal_UCT_othello_agent.py
@@ -1,6 +1,17 @@
+import tensorflow as tf
+from neuronal_network import board_to_tensor
 from UCT_othello_agent import UCTOtelloAgent
+
 from othello import OthelloGame
 
+model = tf.keras.models.load_model("othello_training_model.h5")
+
 class UCTOtelloAgent(UCTOtelloAgent):
+  
 
   def default_policy(self, game: OthelloGame, player):
+    processed_state = board_to_tensor(game.state)
+    predictions = model.predict(processed_state, verbose=0)
+    value = predictions[0][0]
+    return value
+
