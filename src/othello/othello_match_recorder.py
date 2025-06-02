9eb65f14d92cb702714500ca29cddbc9d6676852
Pepe Coral <coritochico@gmail.com>
2025-06-02 21:51:37 +0200

    fix: :bug: changed logic for labling

--- src/othello/othello_match_recorder.py
+++ src/othello/othello_match_recorder.py
@@ -24,8 +24,7 @@ for i in tqdm(range(n)):
     if game.get_valid_moves(current_player) != []:
         active_player = player1 if current_player==1 else player2
         move = active_player.choose_move(game)
-        #print(f"Player {current_player} played {move}")
-        game = game.play_move(move, current_player)    
+        game = game.play_move(move, current_player)
         game_states.append((copy.deepcopy(game.board), current_player))
 
     if current_player == 1:
@@ -38,15 +37,9 @@ for i in tqdm(range(n)):
   for board, player in game_states:
     if white_points == black_points:
         label = 0
-    elif (player == 1 and white_points > black_points and i%2 == 0):
-        label = +1
-    elif (player == 2 and black_points > white_points and i%2 == 1):
-       label = +1
     else:
-        label = -1
+        winner = 1 if white_points > black_points else -1
+        label = winner if player == 1 else -winner
     labeled_states.append((board, label))
 
 np.save("labeled_game_states.npy", np.array(labeled_states, dtype=object))
-  
- 
-
